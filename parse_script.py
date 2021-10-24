f466e630d04449aded6a856d251714d2d312b729
skbif-pc\skbif <skbif@mail.ru>
2021-10-24 23:50:37 +0300

    ver.2 Reads a word to search as an argument from the console, translates characters for input into python regex

--- parse_script.py
+++ parse_script.py
@@ -2,31 +2,34 @@ import os, sys
 import argparse
 import re
 
-#hardcoded word
-mask = 'Error'
 files = []
 
-def work_with_file(path):
+def work_with_file(path, search):
     check_is_file = os.path.isfile(path)
     if check_is_file == True:
         try:
-            read_and_write(path)
+            read_and_write(path, search)
         except PermissionError as err:
             print(f'Permission denied: {err}')
 
-def read_and_write(path):
+def read_and_write(path, search):
     with open(path, 'r', encoding='utf-8') as input_file:
         for line in input_file:
-            if mask in line:
+            if re.findall(f"{search}", line):
                 print(line)
 
 def createParser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--path', nargs='+', default=[''], help='path to file or path to dir')
+    parser.add_argument('-s', '--search', nargs='+', default=['[E|e]rror'], type=str, help='use "?" as one character or "*" as several')
     return parser
 
+def mask(search):
+    search = search.replace("?", ".")
+    search = search.replace("*", "\w+")
+    return search
 
-def check_file(path):
+def check_file(path, search):
     check_file = os.path.exists(path) #check  is path in arg exists
     if check_file == True:
         print(f'{path} exists')
@@ -34,7 +37,7 @@ def check_file(path):
             print('is file')
             file_name, file_name_res = os.path.splitext(path)
             if re.match('\.log$|\.txt', str(file_name_res)): #other formats are unlikely
-                work_with_file(path)
+                work_with_file(path, search)
         elif os.path.isdir(path) == True: #if it's a dir search for files in dir
             print('is dir')
             try:
@@ -43,7 +46,7 @@ def check_file(path):
                         files.append(os.path.join(path, file_name))
                 try:
                     for file_name in files:
-                        work_with_file(file_name)
+                        work_with_file(file_name, search)
                 except FileNotFoundError as e:
                     print(e)
             except PermissionError as err:
@@ -55,4 +58,6 @@ parser = createParser()
 namespace = parser.parse_args(sys.argv[1:])
 
 for name in namespace.path:
-    check_file(name)
+    for search in namespace.search:
+        search = mask(search)
+        check_file(name, search)
